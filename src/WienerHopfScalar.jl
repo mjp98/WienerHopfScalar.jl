55e11b0da0b57658d778f7899b63fab9208b8292
Matthew Priddin <mjp98@cam.ac.uk>
2022-08-24 15:29:41 +0100

    Add dependencies

--- src/WienerHopfScalar.jl
+++ src/WienerHopfScalar.jl
@@ -1,9 +1,19 @@
 module WienerHopfScalar
 
+using ApproxFun
 using HolomorphicFun
 using MacroTools
+using PolynomialRoots
+using SingularIntegralEquations
+using StaticArrays
+using StaticUnivariatePolynomials
+using UnPack
 
+import ApproxFun: domain, space, ncoefficients, coefficients, setcanonicaldomain, evaluate
+import Base: +, -, *, /, inv, literal_pow, getindex, angle, exponent, ==, convert
+import HolomorphicFun: poles, roots
 import MacroTools: postwalk, @capture
+import SpecialFunctions: gamma
 
 export WienerHopfKernel
 export @wienerhopf
