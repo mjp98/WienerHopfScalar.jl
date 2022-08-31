caf7f1ea66b3c616e7c02a0359804b5248d6efea
Matthew Priddin <mjp98@cam.ac.uk>
2022-08-31 21:36:10 +0100

    Refactor index and logfactorise

--- src/generic/index.jl
+++ src/generic/index.jl
@@ -1,8 +1,8 @@
 function isolate_index(
     n::Int,
-    innerpoint= defaultpoint(true),
-    outerpoint = defaultpoint(false)
-    )
+    innerpoint=defaultpoint(true),
+    outerpoint=defaultpoint(false)
+)
 
     n > 0 && return ScalarRational(innerpoint * ones(n), outerpoint * ones(n))
     n < 0 && return ScalarRational(outerpoint * ones(-n), innerpoint * ones(-n))
@@ -10,91 +10,50 @@ function isolate_index(
     return factorise(ScalarConstant(1))
 end
 
-# Refactor this into smaller functions
-
-# struct BranchJump{T<:Number}
-#     x::T
-#     n::Int
-# end
-
-# struct BranchData{T}
-#     jumps::StructVector{BranchJump{T}}
-# end
-
-# BranchData(a, b) = StructArray{BranchJump{eltype(a)}}((a, b))
-
-# jumps(d::BranchData) = d.jumps
-# windingnumber(d::BranchData) = sum(jumps(d).n)
-
-# function logbranchdata(f::Fun; tol=1e-6)
-#     if domain(f) !== canonicaldomain(f)
-#         canonicalf = setcanonicaldomain(copy(f))
-#         return logbranchdata(canonicalf; tol, ε)
-#     end
-#     realF, imagF = reim(F)
-#     x = ApproxFun.roots(imagF)
-#     filter!(z -> abs(z) < 1 - sqrt(tol), x)  # remove endpoints
-#     filter!(z -> realF(z) < 0, x)            # log branch on negative real axis
-
-#     any(abs(z) < tol for z in x) && @warn "possible branch cut at origin"
-
-#     sort!(x)                                 # sort
-#     Δ = [lsign(imagF(leftvalue(z, tol)) < 0) for z in x]
-#     return BranchData(x, cumsum(Δ))
-# end
-
-# struct ContinuousLog{T<:Fun,S<:BranchData}
-#     f::T
-#     data::S
-# end
-# @forward ContinuousLog.data jumps
-
-# function evaluate(f::ContinuousLog, z)
-#     fz = f.f(z)
-#     for (; x, n) in jumps(f)
-#         z < x && return log(fz) - n * i2π
-#     end
-#     return log(fz)
-# end
-
-# function continuouslog(logf::Fun,data::BranchData)
-#     if any(n != 0 for n in data.jumps.n)
-#         @warn "Branch cut crossed, fixing..."
-#         F = ApproxFun.setcanonicaldomain(Fun(f, sp...))
-#         function logf(z)
-#             for (x, n) in zip(xbranch, nbranch)
-#                 z < x && return log(F(z)) - n * i2π
-#             end
-#             return log(F(z))
-#         end
-#         c = Fun(logf, space(F), ncoefficients(F))
-#     end
-
-# end
-
-
-
-function windingdata(f, sp...; ε=1e-6, tol = 1e-6)
-    F = ApproxFun.setcanonicaldomain(copy(Fun(f, sp...)))
+function logbranchdata(f::Fun; ε=1e-6)
+    F = ApproxFun.setcanonicaldomain(copy(f))
     Fr, Fi = reim(F)
-    xbranch = sort(ApproxFun.roots(Fi))
-    # remove endpoints.
-    filter!(z -> abs(z) < 0.9999, xbranch)
+    x = ApproxFun.roots(Fi)
+    filter!(z -> abs(z) < 0.9999, x)  # remove endpoints
+    filter!(z -> Fr(z) < 0, x)         # log branch on negative real axis
+    sort!(x)                           # sort
 
-    # log branch on negative real axis
-    filter!(z -> Fr(z) < 0, xbranch)
-    # jump in branch across cut
+    any(abs(z) < ε for z in x) && @warn "possible branch cut at origin"
 
-
-   # x = logbranchjumps(f; tol)
-
-    nbranch = zeros(Int, length(xbranch) + 1)
-    for (i, x) in enumerate(xbranch)
-        abs(x) < ε && @warn "possible branch cut at origin"
-        # Δ = log(f(rightvalue(x,ε))) - log(f(leftvalue(x,ε)))
-        #nbranch[i+1] = nbranch[i] + Int(round(Δ/(2π*im)))
-        nbranch[i+1] = nbranch[i] + lsign(Fi(leftvalue(x, ε)) < 0)
+    n = zeros(Int, length(x) + 1)
+    for (i, z) in enumerate(x)
+        n[i+1] = n[i] + lsign(Fi(leftvalue(z, ε)) < 0)
     end
-    windingnumber = last(nbranch)
-    return windingnumber, xbranch, nbranch, F
+    x = [-Inf; x]
+    return BranchData(x, n)
 end
+
+struct BranchData{T}
+    jumps::StructVector{BranchJump{T}}
+end
+
+BranchData(a, b) = BranchData(StructArray{BranchJump{eltype(a)}}((a, b)))
+
+jumps(d::BranchData) = d.jumps
+windingnumber(d::BranchData) = last(jumps(d).n)
+
+struct BranchJump{T<:Number}
+    x::T
+    n::Int
+end
+
+struct ContinuousLog{T<:Fun,S<:BranchData}
+    f::T
+    data::S
+end
+@forward ContinuousLog.data jumps
+
+function evaluate(f::ContinuousLog, z)
+    fz = f.f(z)
+    for (; x, n) in jumps(f)
+        z < x && return log(fz) - n * i2π
+    end
+    return log(fz)
+end
+
+(f::ContinuousLog)(z) = evaluate(f, z)
--- src/generic/log.jl
+++ src/generic/log.jl
@@ -1,93 +1,28 @@
-# expcauchy(f::Fun, z) =  exp(complex(cauchy(f, z)))
-# abstract type AbstractLogSplit end
-
-
-
-# """
-
-# """
-# struct LogFun{T} <: AbstractLogSplit
-#     f::T
-# end
-# LogFun(f::Function, sp, args...) = LogFun(Fun(z->log(f(z)), sp, args...))
-# expcauchy(f::LogFun, z) = expccauchy(f,f,z)
 # @forward LogFun.f (isabove, coefficients, ncoefficients, space, domain)
+# @forward SplitNormalised.logf (isabove, coefficients, ncoefficients, space, domain, expcauchy)
 
-# """
-
-# """
-# struct LogSplitKernel{T,S<:AbstractLogSplit} <: WienerHopfKernel
-#     f::T
-#     splitlog::S
-# end
-# splitlog(K::LogSplitKernel) = K.splitlog
-
-# # function evaluate(K:::LogSplitKernel,z)
-# #     return evaluate()
-# @forward LogSplitKernel.splitlog (isabove, coefficients, ncoefficients, space, domain, expcauchy)
-
-# function evaluate(f::LogSplitKernel, z, u)
-#     above = isabove(f, z)
-#     (above && u) && return expcauchy(f, z)
-#     (above && !u) && return f(z) / expcauchy(f, z)
-#     (!above && u) && return f(z) * expcauchy(f, z)
-#     (!above && !u) && return inv(expcauchy(f, z))
-# end
-
-
-# function logfactorise(f, sp, args...; z₋=-10.0im, z₊=10.0im + 10)
-#     windingnumber, xbranch, nbranch, F = windingdata(f, sp,args...)
-#     @info "winding_number(K,Γ) = $windingnumber"
-#     if windingnumber != 0
-#         @info "isolating winding_number"
-#         w = isolate_index(windingnumber, z₊, z₋)
-#         return logfactorise(f / w, sp, args...) * factorise(w, sp)
-#     end
-#     if any(n != 0 for n in nbranch)
-#         @warn "Branch cut crossed, fixing..."
-#         F = ApproxFun.setcanonicaldomain(Fun(f, sp,args...))
-#         function logf(z)
-#             for i in zip(xbranch, nbranch)
-#                 x, n = i
-#                 z < x && return log(F(z)) - n * i2π
-#             end
-#             return log(F(z))
-#         end
-#         c = Fun(logf, space(F), ncoefficients(F))
-#         L = LogFun(Fun(sp, coefficients(c)))
-#         return LogSplitKernel(f, L)
-#     end
-#     return LogSplitKernel(f, LogFun(f, sp))
-# end
-
-
-
-# function continuouslog(f,sp...)
-
-
-# end
-
+expcauchy(f::Fun, z) = exp(complex(cauchy(f, z)))
 
 abstract type AbstractLogSplit end
 struct LogFun{T<:Fun} <: AbstractLogSplit
     f::T
 end
-expcauchy(L::LogFun, z) = exp(complex(cauchy(L.f, z)))
+expcauchy(L::LogFun, z) = expcauchy(L.f, z)
 isabove(L::LogFun, z) = isabove(L.f, z)
 
 LogFun(f::Function, sp...) = LogFun(Fun(log ∘ f, sp...))
 
-struct LogSplitKernel{T,S<:AbstractLogSplit} <: WienerHopfKernel
+struct SplitNormalised{T,S<:AbstractLogSplit} <: WienerHopfKernel
     f::T
-    splitlog::S
+    logf::S
 end
 
-evaluate(K::LogSplitKernel, z) = K.f(z)
-splitlog(K::LogSplitKernel) = K.splitlog
-expcauchy(K::LogSplitKernel, z) = expcauchy(splitlog(K), z)
-isabove(K::LogSplitKernel, z) = isabove(splitlog(K), z)
+evaluate(K::SplitNormalised, z) = K.f(z)
+logf(K::SplitNormalised) = K.logf
+expcauchy(K::SplitNormalised, z) = expcauchy(logf(K), z)
+isabove(K::SplitNormalised, z) = isabove(logf(K), z)
 
-function evaluate(f::LogSplitKernel, z, u)
+function evaluate(f::SplitNormalised, z, u)
     above = isabove(f, z)
     (above && u) && return expcauchy(f, z)
     (above && !u) && return f(z) / expcauchy(f, z)
@@ -97,7 +32,7 @@ end
 
 for op in [:coefficients, :ncoefficients, :space, :domain]
     @eval $op(L::LogFun) = $op(L.f)
-    @eval $op(L::LogSplitKernel) = $op(splitlog(L))
+    @eval $op(L::SplitNormalised) = $op(logf(L))
 end
 
 function logfactorise(
@@ -105,28 +40,28 @@ function logfactorise(
     sp...;
     innerpoint=defaultpoint(true),
     outerpoint=defaultpoint(false)
-    )
+)
 
-    windingnumber, xbranch, nbranch, F = windingdata(f, sp...)
-    @info "winding_number(K,Γ) = $windingnumber"
-    if windingnumber != 0
-        @info "isolating winding_number"
-        w = isolate_index(windingnumber, innerpoint, outerpoint)
-        return logfactorise(f / w, sp...) * factorise(w, sp[1])
+    F = ApproxFun.setcanonicaldomain(Fun(f, sp...))
+
+    branchdata = logbranchdata(F)
+
+    winding_number = windingnumber(branchdata)
+
+    @info "winding_number(K,Γ) = $winding_number"
+
+    if !iszero(winding_number)
+        @info "winding_number non-zero: isolating..."
+        w = isolate_index(winding_number, innerpoint, outerpoint)
+        return logfactorise(f / w, sp...) * factorise(w, first(sp))
     end
-    if any(n != 0 for n in nbranch)
-        @info "Branch cut crossed, fixing..."
-        F = ApproxFun.setcanonicaldomain(Fun(f, sp...))
-        function logf(z)
-            for i in zip(xbranch, nbranch)
-                x, n = i
-                z < x && return log(F(z)) - n * i2π
-            end
-            return log(F(z))
-        end
-        c = Fun(logf, space(F), ncoefficients(F))
+
+    if any(n != 0 for n in jumps(branchdata).n)
+        continuouslogf = ContinuousLog(F, branchdata)
+        c = Fun(continuouslogf, space(F), ncoefficients(F))
         L = LogFun(Fun(first(sp), coefficients(c)))
-        return LogSplitKernel(f, L)
+        return SplitNormalised(f, L)
     end
-    return LogSplitKernel(f, LogFun(f, sp...))
+
+    return SplitNormalised(f, LogFun(f, sp...))
 end
